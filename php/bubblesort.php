04f0e18a5d4ad69945c134879b15a4f4ac4bca30
zouyi <php-cpm@users.noreply.github.com>
2018-02-19 10:52:47 +0800

    Update bubblesort.php
    code style format
    add 2nd method

--- php/bubblesort.php
+++ php/bubblesort.php
@@ -1,29 +1,60 @@
 <?php
 /**
- * Bubble Sort is one easist sort method
- * given an unsorted list 5,3,8,6,4
- * diff each form beginning and change position
- * of the bigger one, do it untill everything
- * sorted
+ * @file bubbleSort.php
+ * Bubble Sort is one easiest sort method
+ * given an unsorted list such as 5,3,8,6,4
+ * diff each number from the beginning to the end and change
+ * position of the bigger one, do it until everything sorted
  */
- 
- $list = [5,3,8,6,4];
- echo "before: \n";
- print_r($list);
- function bubbleSort($list){
- 
- for ($j=0; $j<count($list)-1; $j++){
-   for ($i=0; $i<count($list)-1; $i++) {
-     if ($list[$i+1] < $list[$i]) {
-       $tmp = $list[$i+1];
-       $list[$i+1] = $list[$i];
-       $list[$i] = $tmp;
-       unset($tmp);
-     }
-   }
- }
- return $list;
- }
- 
- echo "\n after: \n";
- print_r(bubbleSort($list));
+
+/**
+ * original
+ *
+ * @var array $list
+ * @return array
+ */
+function bubbleSort1(array $list)
+{
+    for ($j = 0; $j < count($list) - 1; $j++) {
+        for ($i = 0; $i < count($list) - 1; $i++) {
+            if ($list[$i + 1] < $list[$i]) {
+                $tmp = $list[$i + 1];
+                $list[$i + 1] = $list[$i];
+                $list[$i] = $tmp;
+                unset($tmp);
+            }
+        }
+    }
+
+    return $list;
+}
+
+/**
+ * loop less
+ *
+ * after each loop, the biggest number is stable
+ *
+ * @var array $list
+ * @return array
+ */
+function bubbleSort2(array $list)
+{
+    for ($j = 0; $j < count($list) - 1; $j++) {
+        for ($i = 0; $i < count($list) - $j - 1; $i++) {
+            if ($list[$i + 1] < $list[$i]) {
+                $tmp = $list[$i + 1];
+                $list[$i + 1] = $list[$i];
+                $list[$i] = $tmp;
+                unset($tmp);
+            }
+        }
+    }
+
+    return $list;
+}
+
+$list = [5, 3, 8, 6, 4];
+echo "before: \n";
+print_r($list);
+echo "\n after: \n";
+print_r(bubbleSort2($list));
